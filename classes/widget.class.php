3997ff7334a2dfb185dd9c32c7b2f5c0bacf16fc
Joakim Nyquist <joakim@develoopab.se>
2011-12-06 15:29:14 +0100

    nya filer

--- classes/widget.class.php
+++ classes/widget.class.php
@@ -9,16 +9,21 @@
 		}
 		public function getInfo($id) {
 			$info = $this->sql->_array("SELECT * FROM bloggare WHERE id = '$id'");
-			return $info;
+			$json = '{"id":"' . $info[0]['id'] . '","namn":"' . $info[0]['namn'] . '","bild":"' . $info[0]['bild'] . '","insamling":"' . $info[0]['insamling'] . '","url":"' . $info[0]['url'] . '"}';
+			return $json;
 		}
 				
 	}
-	$widget = new widget;
-	$widgetInfo = $widget->getInfo($_GET['id']);
-	//Nycklar
-	// id - namn - bild - insamling - url
-	//!!!! OBS, INSAMLING kommer bytas ut mot SALDO när jag fått tag på min lokaladatabasdump
+	if(isset($_GET['id']) && !empty($_GET['id'])) {
+		//Nycklar
+		// id - namn - bild - insamling - url
+		//!!!! OBS, INSAMLING kommer bytas ut mot SALDO när jag fått tag på min lokaladatabasdump
+		$widget = new widget;
+		$widgetInfo = $widget->getInfo($_GET['id']);	
+	} else {
+		$widgetInfo = '{"error":"No id"}';
+	}
 	//echo "<img src=\"../images/bloggare/" . $widgetInfo[0]['bild'] . "\" />";
-	return $widgetInfo;
+	echo $widgetInfo;
 
 ?>
